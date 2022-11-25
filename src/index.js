9aabe1eeea4ec917fa14b94b2dd5837e32c5264f
Tung Dao <tungdao17@gmail.com>
2022-11-25 12:02:50 +0800

    Add HashRouter

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import GA4React from "ga-4-react";
 import App from "./App";
+import { HashRouter as Router } from "react-router-dom";
 
 const ga4react = new GA4React("G-4ZFLENPHX1");
 
@@ -12,6 +13,10 @@ const ga4react = new GA4React("G-4ZFLENPHX1");
     .catch((err) => console.log("Analytics Failure"))
     .finally(() => {
       const root = ReactDOM.createRoot(document.getElementById("root"));
-      root.render(<App />);
+      root.render(
+        <Router>
+          <App />
+        </Router>
+      );
     });
 })();
