aeba783d6e1d79f62efab42eedddfafc10b32779
Tung Dao <tungdao17@gmail.com>
2022-12-12 14:00:27 +0800

    Update registration

--- components/registration_form/index.jsx
+++ components/registration_form/index.jsx
@@ -4,8 +4,14 @@ import styles from './registration_form.module.scss';
 const apiUrl = 'https://api.aihub.ml/v1/mcc/links';
 
 const RegistrationForm = () => {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [code, setCode] = useState('');
+  const [allowContact, setAllowContact] = useState(false);
+
+  const onNameChange = (e) => {
+    setName(e.target.value);
+  };
 
   const onEmailChange = (e) => {
     setEmail(e.target.value);
@@ -15,7 +21,16 @@ const RegistrationForm = () => {
     setCode(e.target.value);
   };
 
+  const onAllowContactChange = (e) => {
+    setAllowContact(!allowContact);
+
+  }
+
   const onSubmit = () => {
+    if (name === '') {
+      alert('Name cannot be empty.');
+      return;
+    }
     if (email === '') {
       alert('Email cannot be empty.');
       return;
@@ -24,8 +39,12 @@ const RegistrationForm = () => {
       alert('Access code cannot be empty.');
       return;
     }
+    if (allowContact === false) {
+      alert('You need to allow us to contact you in order to send the download link to your email.');
+      return;
+    }
 
-    const body = JSON.stringify({email: email, code: code});
+    const body = JSON.stringify({name: name, email: email, code: code});
     fetch(apiUrl, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
@@ -41,10 +60,19 @@ const RegistrationForm = () => {
   return (
     <>
       <div className={styles.input}>
+        <input placeholder="*Name" onChange={onNameChange} />
+      </div>
+      <div className={styles.input}>
         <input placeholder="*Email" onChange={onEmailChange} />
       </div>
       <div className={styles.input}>
         <input placeholder="*Access Code" onChange={onAccessCodeChange} />
+      </div>
+      <div className={styles.checkbox}>
+        <label>
+          <input type="checkbox" onChange={onAllowContactChange} />
+          <i>Allow us to contact you via your email</i>
+        </label>
       </div>
       <div className={styles.submit} onClick={onSubmit}>
         Register
