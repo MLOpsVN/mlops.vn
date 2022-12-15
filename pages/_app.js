122abb49c97f78a8dff90031543b7d9c4b855257
Tung Dao <tungdao17@gmail.com>
2022-12-15 17:32:26 +0800

    Add gg analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,20 @@
+import GA4React from 'ga-4-react';
 import '../styles/globals.css';
 import Layout from '@/core/layout';
 import Head from 'next/head';
 
+const ga4react = new GA4React('G-4ZFLENPHX1');
+
 function MyApp({ Component, pageProps }) {
+  ga4react
+    .initialize()
+    .then((res) => console.log('Analytics Success'))
+    .catch((err) => console.log('Analytics Failure'));
   return (
     <div>
       <Head>
         <title>MLOpsVN</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Layout>
         <Component {...pageProps} />
